a7f0d92b6838921d435f7cf25b1e12c5b158089d
Gustavo Wilker <gustavowilkeravelar@gmail.com>
2022-04-02 01:04:11 -0300

    Aprendendo sobre Statelesswidget

--- app_teste/lib/main.dart
+++ app_teste/lib/main.dart
@@ -1,12 +1,26 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 main() {
-  runApp(Container(
-    child: Center(
-      child:  Text('Flutterando', textDirection: TextDirection.ltr,
-    style: TextStyle(color: Colors.white, fontSize: 50.0),
-    ),
-    ),
+  runApp(AppWidget(
+    title: 'Flutterando 2',
   ));
+}
+
+class AppWidget extends StatelessWidget {
+  final String title;
+
+  const AppWidget({Key? key, required this.title}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+          textDirection: TextDirection.ltr,
+          style: TextStyle(color: Colors.white, fontSize: 50.0),
+        ),
+      ),
+    );
+  }
 }
