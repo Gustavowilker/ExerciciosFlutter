817df64d5a3e60346ac8003fb405bc295a6f5ee7
Gustavo Wilker <gustavowilkeravelar@gmail.com>
2022-04-02 02:08:35 -0300

    organizando as pastas
    separando cada widget em pastas para o código ficar mais limpo e organizado

--- app_teste/lib/main.dart
+++ app_teste/lib/main.dart
@@ -1,48 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'app_widget.dart';
+
 main() {
   runApp(AppWidget(
     title: 'Flutterando 2',
   ));
-}
-
-class AppWidget extends StatelessWidget {
-  final String title;
-
-  const AppWidget({Key? key, required this.title}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatefulWidget {
-  @override
-  State<HomePage> createState() {
-    return HomePageState();
-  }
-}
-
-class HomePageState extends State<HomePage> {
-  int counter = 0;
-
-  @override
-  Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: GestureDetector(
-          child: Text('Contador: $counter'),
-          onTap: () {
-            setState(() {
-              counter++;
-            });    
-          },
-        ),
-      ),
-    );
-  }
 }
