2a2268fcd822298ef4e5952794ea8066e95d2baf
Gustavo Wilker <gustavowilkeravelar@gmail.com>
2022-04-03 17:38:33 -0300

    praticando Stack
    fit: BoxFit.cover (propriedade que estica a imagem no tamanho todo)
    
    color: Colors.black.withOpacity()  (Propriedade que cria uma máscara que nesse caso é preta.

--- app_teste/lib/login_page.dart
+++ app_teste/lib/login_page.dart
@@ -10,64 +10,85 @@ class _LoginPageState extends State<LoginPage> {
   String email = '';
   String password = '';
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: SingleChildScrollView(
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width,
-          height: MediaQuery.of(context).size.height,
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-
-                Container(
-                  width: 200,
-                  height: 200,
-                  child: Image.network('https://executivalab.com.br/wp-content/uploads/2021/07/modelo-de-logomarca-simbolo-apple.png')),
-
-                  Container(height: 20 ,),
-
-                TextField(
-                  onChanged: (Text) {
-                    email = Text;
-                  },
-                  keyboardType: TextInputType.emailAddress,
-                  decoration: InputDecoration(
-                      labelText: 'Email', border: OutlineInputBorder()),
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                TextField(
-                  onChanged: (Text) {
-                    password = Text;
-                  },
-                  obscureText: true,
-                  decoration: InputDecoration(
-                    labelText: 'Password',
-                    border: OutlineInputBorder(),
+  Widget _body() {
+    return Column(
+      children: [
+        SingleChildScrollView(
+          child: SizedBox(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height,
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Container(
+                      width: 200,
+                      height: 200,
+                      child: Image.asset('assets/imagens/LOGO_PRETO.png')),
+                  Container(
+                    height: 20,
                   ),
-                ),
-                SizedBox(
-                  height: 15,
-                ),
-                RaisedButton(
-                  onPressed: () {
-                    if (email == "gustavo wilker" && password == '123') {
-                      Navigator.of(context).pushReplacementNamed('/home');
-                    } else {
-                      print('Login inválido');
-                    }
-                  },
-                  child: Text('Entrar'),
-                ),
-              ],
+                  TextField(
+                    onChanged: (Text) {
+                      email = Text;
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        labelText: 'Email', border: OutlineInputBorder()),
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  TextField(
+                    onChanged: (Text) {
+                      password = Text;
+                    },
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      labelText: 'Password',
+                      border: OutlineInputBorder(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 15,
+                  ),
+                  RaisedButton(
+                    onPressed: () {
+                      if (email == "gustavo wilker" && password == '123') {
+                        Navigator.of(context).pushReplacementNamed('/home');
+                      } else {
+                        print('Login inválido');
+                      }
+                    },
+                    child: Text('Entrar'),
+                  ),
+                ],
+              ),
             ),
           ),
         ),
+      ],
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height,
+            child: Image.asset(
+              'assets/imagens/back_logo.jpg',
+              fit: BoxFit.cover,
+            ),
+          ),
+          Container(
+            color: Colors.black.withOpacity(0.1),
+          ),
+          _body(),
+        ],
       ),
     );
   }
