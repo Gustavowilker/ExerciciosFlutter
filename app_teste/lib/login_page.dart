624c1ea28524b016316ebd1ac5ae902d222ad7dd
Gustavo Wilker <gustavowilkeravelar@gmail.com>
2022-04-03 15:30:01 -0300

    criando uma tela de login
    criando tela de login e aprendendo navegação entre telas. também sobre: SigleChildScrollView (para o corpo ter scrool),
    SizeBox(para criar campos com widgets ou só fazer espaços entre eles),  MediaQuery.of(context).size.width (para viabilizar as larguras do SizeBox para scroll no Scaffold),
    
    MainAxisAlignment: (para alinhar o eixo)
    
    padding (como pai do Column para fazer espaço nas laterais)
    
    onChanged: (para fazer leitura do TextField e acrescentar na variável).
    
    decoration: (para colocar um label, bordas e customizações no TextField)
    
    obscureText: true (para textField de senhas)
    
    Navigator.of(context).push  (para mudar de página,
    podendo adicionar 'Replacement' para não voltar para tela anterior,
    e 'Named' para rotas de navegação.

---
+++ app_teste/lib/login_page.dart
@@ -0,0 +1,66 @@
+import 'package:app_teste/home_page.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  String email = '';
+  String password = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                TextField(
+                  onChanged: (Text) {
+                    email = Text;
+                  },
+                  keyboardType: TextInputType.emailAddress,
+                  decoration: InputDecoration(
+                      labelText: 'Email', border: OutlineInputBorder()),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                TextField(
+                  onChanged: (Text) {
+                    password = Text;
+                  },
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    labelText: 'Password',
+                    border: OutlineInputBorder(),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                RaisedButton(
+                  onPressed: () {
+                    if (email == "gustavo wilker" && password == '123') {
+                      Navigator.of(context).pushReplacementNamed('/home');
+                    } else {
+                      print('Login inválido');
+                    }
+                  },
+                  child: Text('Entrar'),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
